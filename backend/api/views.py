4624a13473a0daab0f1b81416158a9b87a0c674f
hamid_rd3 <h.dashtabadi@gmail.com>
2022-09-02 11:15:57 +0000

    api view is working

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,16 @@
-from . import views
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
-urlpatterns = [
-    path('', views.api_home),
-    path('auth/', obtain_auth_token),
-]
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from products.serializer import QoSSerializer
+from products.models import QoS
+from time import time
+
+
+@api_view(["GET", "POST"])
+def api_home(request, *args, **kwargs):
+    start = time()
+    instance = QoS.objects.all().last()
+    data = QoSSerializer(instance).data
+    end = time()
+    data["total_extraction_time"] = end-start
+    return Response(data)

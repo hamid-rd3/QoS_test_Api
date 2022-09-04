5fb7fa3fb135e5df05810cdfe4f4a1276f6a4dcf
hamid_rd3 <h.dashtabadi@gmail.com>
2022-09-04 13:09:16 +0000

    delete useless accounts and logins

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,33 +2,34 @@ from . import views
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import permissions
-
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
 from drf_yasg import openapi
+from .views import UserDetailAPI, RegisterUserAPIView
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
         title="QoS API",
+        url="http://185.130.78.174:5201/api/swagger/",
         default_version='1.0.0',
-        description="API documentation \n github link :https://github.com/hamid-rd3/QoS_test_Api",
+        description="quality of service measurement tool using ping,curl,selenium chrome driver and ddosify to extract some parameters such as latency,page load timing,jitter,throughput,hopcount ...",
         contact=openapi.Contact(
             email="h.dashtabadi@gmail.com"),
         terms_of_service="https://www.google.com/policies/terms/",
     ),
     public=True,
-    url="http://185.130.78.174:5201/api/swagger/",
     permission_classes=[permissions.AllowAny],
 
 )
 
 urlpatterns = [
     path('', views.api_home),
-    path('auth/', obtain_auth_token),
-    path('register/', views.RegisterUserAPIView.as_view()),
+    path('auth/', views.obtain_auth_token_api),
     path('swagger-json/', schema_view.without_ui(
         cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
+    path("get-details", UserDetailAPI.as_view()),
+    path('register', RegisterUserAPIView.as_view()),
 ]
--- backend/qoshome/urls.py
+++ backend/qoshome/urls.py
@@ -1,11 +1,9 @@
 
 from django.contrib import admin, admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/products/', include('products.urls')),
-    path("accounts/", include("accounts.urls")),
 ]
